b8f264ab9cd164d9cdce7b75af99b497bed0cd86
clark45767 <clarkigarma@gmail.com>
2026-02-13 12:32:42 +0400

    Add files via upload

--- Activity1.py
+++ Activity1.py
@@ -1,5 +1,14 @@
 number = int(input("Enter any number:"))
-if number%2==0:
+
+if number>=50:
+  print("Number is greater than 50...")
+  if number% 2==0:
     print("Number is even ..")
-else:
+  else:
     print("Number is odd ...")
+else:
+    print("Number is less than 50...")
+    if number% 2==0:
+     print("Number is even ..")
+    else:
+     print("Number is odd ...")
